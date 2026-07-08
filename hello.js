96acc88952e19488c8ec15052cc7045995b7d973
Shem <mongareshem1@gmail.com>
2026-07-08 03:09:20 +0300

    Add revert console log

--- hello.js
+++ hello.js
@@ -4,3 +4,4 @@ console.log('This is a soft reset test!');
 console.log('This is a hard reset test!');
 console.log('This is will be lost after hard reset!');
 console.log('This is will be lost as well!');
+console.log('Trying out revert')
