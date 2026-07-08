b57d9aef1a2811d183d9258f429f0e492d1f5812
Shem <mongareshem1@gmail.com>
2026-07-08 03:00:49 +0300

    Add code that will be lost after a hard reset

--- hello.js
+++ hello.js
@@ -2,3 +2,5 @@ console.log('Hello Git!');
 console.log('Hello GitHub!');
 console.log('This is a soft reset test!');
 console.log('This is a hard reset test!');
+console.log('This is will be lost after hard reset!');
+console.log('This is will be lost as well!');
